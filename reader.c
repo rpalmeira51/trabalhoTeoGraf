25acd7587a20c2f0254aefc96eeb13c17fa7241b
Rodrigo Palmeira <rpalmeira1999@gmail.com>
2019-08-30 21:10:46 -0300

    Lista Funcionando

--- reader.c
+++ reader.c
@@ -14,30 +14,31 @@ main (){
     printf("long %ld\n", sizeof(c));
 }
 */
-char** readMatrix(unsigned long v_number);
-
 typedef struct node
 {
-    PointerNode** value; //Aponta para a posição do elemento no array
-    PointerNode* next; //Aponta para o próximo elemento da lista
+    struct node** value; //Aponta para a posição do elemento no array
+    struct node* next; //Aponta para o próximo elemento da lista
 } PointerNode;
 
 typedef struct int_node
 {
     /* data */
     unsigned int value;
-    IntNode* next;
+    struct int_node* next;
 
 } IntNode;
 
+char** readMatrix(unsigned long v_number);
 
+IntNode** read_list_int(unsigned long v_number);
+void print_list_int(IntNode* p);
 
 
 int main (int argc,char** argv ){
     unsigned long v_number;
     scanf("%lu", &v_number);
     printf("%lu\n",v_number);
-    char** matrix = readMatrix(v_number);
+    //char** matrix = readMatrix(v_number);
     /*
     for (int i= 0;  i< v_number; i++){
         for(int j = (i+1); j< v_number;j++ ){ 
@@ -46,15 +47,13 @@ int main (int argc,char** argv ){
     */
     IntNode** list = read_list_int(v_number);
     IntNode* p;
-    printf("[ ");
+    printf("[ \n");
     for(int i = 0; i<v_number; i++){
        printf("%d - > ",i);
-       for(p = list[i]; p=! NULL; p = p->next){
-           printf("%d ", )
-       }
-       printf("\n")
-   }
-    printf("[ ");
+       print_list_int(list[i]);
+    }
+    printf("] ");
+    
     return 0;
 }
 
@@ -72,7 +71,7 @@ char** readMatrix(unsigned long v_number){
     }
     int a,b;
     while((fscanf(stdin,"%d %d",&a,&b)) != EOF){
-        printf("a= %d b = %d\n",a,b);
+       // printf("a= %d b = %d\n",a,b);
         (a<=b) ? (p[a][b-a-1] =1 ) : (p[b][a-b-1]=1); 
             
     }
@@ -80,32 +79,52 @@ char** readMatrix(unsigned long v_number){
 
 }
 
+void print_list_int(IntNode* p){
+    printf("Lista ->  ");
+    while (p!= NULL){
+        printf("%d ", p->value);
+        p = p->next;
+    }
+    printf("\n");
+
+}
+
 // Lembrar ponteiros são passados por valor 
 //Bota o ponteiro na lista
 IntNode* put_inode(IntNode* p ,unsigned int value){
-    IntNode new_node = { 
-        value,
-        p
-    };
-    p = &new_node;
-    return p;
+    IntNode* new_pointer;
+    if (new_pointer = (IntNode *) malloc(sizeof(IntNode))){
+        new_pointer->value = value;
+        new_pointer->next = p;
+        ///printf("Ponteiro antes p :%p\n",p);
+        ///printf("Ponteiro depois p :%p\n",p);
+        ///printf("O que eu estou botando %u %p \n",new_pointer->value, new_pointer);
+        return new_pointer;
+    }
+    exit(1);
      
 }
 
 IntNode** read_list_int(unsigned long v_number){
     IntNode** p;
-    p = calloc(v_number,sizeof(IntNode *)*v_number);
+    p = (IntNode **) calloc(v_number,sizeof(IntNode *)*v_number);
     IntNode** pointer;
-    int a,b;
+    unsigned int a,b;
     while((fscanf(stdin,"%d %d",&a,&b)) != EOF){
+        printf("Fazendo a %d  e b %d \n",a,b);
         p[a] = put_inode(p[a],b);
-        p[b] = put_inode(p[b],a);         
+        p[b] = put_inode(p[b],a);
+        printf("Index %d ",a);
+        print_list_int(p[a]);
+        printf("Index %d ",b);
+        print_list_int(p[b]);
+        //printf("Atualmente p[a] : %u e p[b]: %u \n", p[a]->value,p[b]->value);        
     }
     
     return p;
 }
-
+/*
 PointerNode** read_list_pointer(unsigned long v_number){
     
 }
-
+*/
